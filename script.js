940f0a8d8fc072218afa34e207656e2233135c37
Annabelle <lilacpaw2018@gmail.com>
2023-08-01 10:52:36 -0900

    created user interface

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2
 }
+
+let num1, num2, operator;
+
+function operate(num1, num2, operator){
+    if(operator=='+'){
+        return add(num1, num2)
+    }else if(operator=='-'){
+        return subtract(num, num2)
+    }else if(operator=='*'){
+        return multiply(num1, num2)
+    }else{
+        return divide(num1, num2)
+    }
+}
